97fc993fd9bdf2303c9ee034e2ff5e621130f869
Wiltzu <ville.ahti2@gmail.com>
2013-11-27 11:29:09 +0200

    cucumber

---
+++ src/test/java/fi/ty/selenium/CucumberTest.java
@@ -0,0 +1,18 @@
+package fi.ty.selenium;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+//@CucumberOptions(format = "pretty")
+public class CucumberTest {
+
+//	@Before
+//	public void setUp() throws Exception {
+//		
+//	}
+
+}
